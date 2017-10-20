774e4d289c65bd4e0b9238bc1a8878a44eab023d
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-20 18:39:21 +0300

    Comment in publishing controller

--- src/Publish/FeedbackController.php
+++ src/Publish/FeedbackController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers;
 
 use Runsite\Component\Feedback\Controllers\FeedbackController as BaseFeedbackController;
-use Illuminate\Http\Request;
 
 class FeedbackController extends BaseFeedbackController
 {
-    
+    /*
+    |--------------------------------------------------------------------------
+    | Feedback Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is empty, 
+    | since it is extended 
+    | from Runsite\Component\Feedback\Controllers\FeedbackController. 
+    | 
+    | You can create your own methods here, 
+    | or redeclare existing ones in the parent controller.
+    |
+    | Parent controller is implemented by Feedback interface, 
+    | then must contain two methods:
+    |
+    | public function show();
+    | public function send(Request $request);
+    |
+    */
 }
